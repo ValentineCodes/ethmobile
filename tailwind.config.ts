b3eb8739189a8364590d2d6b0d86cfd4b1899e19
Valentine Orga <valentineorga@gmail.com>
2025-02-25 16:15:24 +0100

    chore: set up Poppins font as default

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ export default {
         foreground: "var(--foreground)",
       },
     },
+    fontFamily: {
+      poppins: ["Poppins", "serif"],
+    },
   },
   plugins: [],
 } satisfies Config;
