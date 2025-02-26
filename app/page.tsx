1a47b2b15b25e4233909cafde9d8430bcee7dd5b
Valentine Orga <valentineorga@gmail.com>
2025-02-26 11:21:33 +0100

    feat: design header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import Header from "@/components/Header";
+import { Popover } from "@headlessui/react";
+
 export default function Home() {
-  return <main>Scaffold-ETH Mobile</main>;
+  return (
+    <main className="text-white">
+      <Popover className="relative min-h-screen w-full bg-[#292f37]">
+        <Header />
+      </Popover>
+    </main>
+  );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,133 @@
+import React, { Fragment } from "react";
+import { RiMenu2Fill } from "react-icons/ri";
+import { CgClose } from "react-icons/cg";
+import { FaLinkedinIn, FaTwitter, FaGithub } from "react-icons/fa";
+
+import { Popover, Transition } from "@headlessui/react";
+import Link from "next/link";
+
+type Props = {};
+
+export default function Header({}: Props) {
+  return (
+    <header className="container mx-auto flex justify-between items-center border-b-[0.5px] border-[gray] bg-transparent p-6 space-x-4 h-24">
+      <h1 className="text-lg font-semibold">📲</h1>
+
+      <nav className="flex items-center space-x-6">
+        <ul className="hidden lg:flex items-center justify-between space-x-6 text-sm font-light">
+          <li>
+            <a href="#about" className="hover:text-[#15f7d6] duration-300">
+              Features
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="hover:text-[#15f7d6] duration-300">
+              Examples
+            </a>
+          </li>
+          <li>
+            <a href="#Projects" className="hover:text-[#15f7d6] duration-300">
+              Docs
+            </a>
+          </li>
+          <li>
+            <a href="#Projects" className="hover:text-[#15f7d6] duration-300">
+              1.0.0
+            </a>
+          </li>
+        </ul>
+
+        <span className="text-4xl text-gray-600 font-thin hidden lg:flex">
+          |
+        </span>
+
+        <div className="hidden sm:flex space-x-6">
+          <Link
+            href="https://www.linkedin.com/in/valentineorga"
+            target="_blank"
+          >
+            <FaLinkedinIn className="text-gray-500 hover:text-[#15f7d6] duration-200 text-2xl" />
+          </Link>
+
+          <Link href="https://x.com/ValentineOrga" target="_blank">
+            <FaTwitter className="text-gray-500 hover:text-[#15f7d6] duration-200 text-2xl" />
+          </Link>
+
+          <Link href="https://github.com/ValentineCodes" target="_blank">
+            <FaGithub className="text-gray-500 hover:text-[#15f7d6] duration-200 text-2xl" />
+          </Link>
+        </div>
+
+        <span className="text-4xl text-gray-600 font-thin block lg:hidden">
+          |
+        </span>
+
+        <Popover.Button className="hover:bg-black hover:bg-opacity-20 p-2 rounded-lg block lg:hidden">
+          {({ open }) =>
+            open ? (
+              <CgClose className="text-xl" />
+            ) : (
+              <RiMenu2Fill className="text-xl" />
+            )
+          }
+        </Popover.Button>
+      </nav>
+
+      <Transition
+        as={Fragment}
+        enter="duration-100 ease-out"
+        enterFrom="scale-95 opacity-0"
+        enterTo="scale-100 opacity-100"
+        leave="duration-75 ease-out"
+        leaveFrom="scale-100 opacity-100"
+        leaveTo="scale-95 opacity-0"
+      >
+        <Popover.Panel
+          as="nav"
+          focus
+          className="absolute inset-x-0 top-24 z-[1000] origin-top-right transform px-2 py-8 transition lg:hidden bg-[#292f37]"
+        >
+          <ul className="text-sm font-light">
+            <li className="p-4 hover:bg-black hover:bg-opacity-20 duration-200">
+              <a href="#about" className="block">
+                About
+              </a>
+            </li>
+            <li className="p-4 hover:bg-black hover:bg-opacity-20 duration-200">
+              <a href="#skills" className="block">
+                Skills
+              </a>
+            </li>
+            <li className="p-4 hover:bg-black hover:bg-opacity-20 duration-200">
+              <a href="#Projects" className="block">
+                Projects
+              </a>
+            </li>
+            <li className="p-4 hover:bg-black hover:bg-opacity-20 duration-200">
+              <a href="#contact" className="block">
+                Contact
+              </a>
+            </li>
+          </ul>
+
+          <div className="flex sm:hidden space-x-6 ml-4 mt-2">
+            <Link
+              href="https://www.linkedin.com/in/valentineorga"
+              target="_blank"
+            >
+              <FaLinkedinIn className="text-gray-500 hover:text-[#15f7d6] duration-200 text-2xl" />
+            </Link>
+
+            <Link href="https://x.com/ValentineOrga" target="_blank">
+              <FaTwitter className="text-gray-500 hover:text-[#15f7d6] duration-200 text-2xl" />
+            </Link>
+
+            <Link href="https://github.com/ValentineCodes" target="_blank">
+              <FaGithub className="text-gray-500 hover:text-[#15f7d6] duration-200 text-2xl" />
+            </Link>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </header>
+  );
+}
