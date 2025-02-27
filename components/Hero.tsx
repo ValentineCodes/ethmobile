d7ba423d15a59c71ca005a650c417b9526c1b511
Valentine Orga <valentineorga@gmail.com>
2025-02-27 20:07:52 +0100

    refactor: adjust font size, font weight, color and content of hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,101 +1,142 @@
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
+
+import { RxClipboardCopy } from "react-icons/rx";
+import { TiTickOutline } from "react-icons/ti";
 
 export default function Hero() {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyCommand = async () => {
+    try {
+      // Copy text to clipboard
+      await navigator.clipboard.writeText(
+        "https://github.com/dewdrip/scaffold-eth-mobile.git",
+      );
+      setIsCopied(true); // Show "Copied!" effect
+
+      // Remove "Copied!" text after 2 seconds
+      setTimeout(() => setIsCopied(false), 2000);
+    } catch (err) {
+      console.error("Failed to copy text:", err);
+    }
+  };
+
   return (
-    <section className="px-6 py-14 max-w-[1200px] mx-auto">
-      <div className="flex flex-col md:flex-row items-center md:items-start justify-between md:justify-between md:space-x-14 md:mt-16">
-        <div>
+    <section className="p-6 max-w-[1200px] mx-auto">
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-between md:justify-between md:space-x-14 md:mt-14">
+        <div className="flex flex-col items-center md:items-start">
           {/* Logo */}
-          <h1 className="text-4xl md:text-5xl font-bold tracking-tight text-white mb-6">
+          <h1 className="text-3xl md:text-5xl text-center md:text-left font-bold tracking-tight mb-6">
             Scaffold-ETH Mobile
           </h1>
 
           {/* Main heading */}
-          <p className="max-w-lg mx-auto text-xl sm:text-2xl text-gray-300 mb-8">
+          <p className="max-w-md mx-auto text-lg sm:text-xl text-center md:text-left text-gray-400 mb-8">
             Build native mobile dApps using{" "}
-            <span className="text-white font-semibold">lightweight</span>,{" "}
-            <span className="text-white font-semibold">composable</span>, and{" "}
-            <span className="text-white font-semibold">type-safe</span> modules
-            that interface with Ethereum
+            <span className="text-black">lightweight</span>,{" "}
+            <span className="text-black">composable</span>, and{" "}
+            <span className="text-black">type-safe</span> modules that interface
+            with Ethereum
           </p>
 
           {/* Action buttons */}
           <div className="flex gap-4 mb-12">
-            <button className="px-6 py-3 bg-orange-500 hover:bg-orange-600 text-white rounded-lg font-medium transition-colors">
+            <button className="px-4 py-2 bg-orange-400 hover:bg-orange-500 rounded-md font-bold transition-colors text-sm text-gray-600">
               Get started
             </button>
-            <button className="px-6 py-3 bg-gray-800 hover:bg-gray-700 text-white rounded-lg font-medium transition-colors">
-              Why SEM?
-            </button>
-            <button className="px-6 py-3 bg-gray-800 hover:bg-gray-700 text-white rounded-lg font-medium transition-colors">
+            <Link
+              href="https://github.com/dewdrip/scaffold-eth-mobile"
+              className="px-4 py-2 bg-gray-100 border hover:bg-gray-200 rounded-md font-bold transition-colors text-sm text-gray-600"
+            >
               GitHub
-            </button>
+            </Link>
           </div>
         </div>
 
         {/* Package manager tabs */}
-        <div>
-          <div className="w-full max-w-[500px] mx-auto bg-gray-800/50 rounded-lg overflow-hidden backdrop-blur-sm">
-            <div className="flex border-b border-gray-700">
-              <button className="px-4 py-2 text-orange-400 border-b-2 border-orange-400 font-medium">
+        <div className="w-full max-w-[500px] flex flex-col items-center md:items-start">
+          <div className="w-full max-w-[500px] mx-auto bg-gray-800/50 rounded-lg overflow-hidden backdrop-blur-sm border-2 border-gray-100">
+            <div className="bg-white">
+              <button className="px-4 py-2 border-b-2 border-orange-400 font-medium">
                 git
               </button>
             </div>
-            <div className="p-4 pb-14 text-left font-mono">
-              <code className="text-gray-300">
-                git clone https://github.com/dewdrip/scaffold-eth-mobile
+            <div className="p-4 pb-4 pr-20 text-left bg-gray-100 relative">
+              <code>
+                <span className="text-purple-500">git</span>{" "}
+                <span className="text-blue-900">
+                  clone https://github.com/dewdrip/scaffold-eth-mobile
+                </span>
               </code>
+
+              <button
+                onClick={copyCommand}
+                className="text-xl text-gray-500 border border-gray-300 hover:bg-gray-200 duration-200 rounded-md p-2 absolute top-4 right-4"
+              >
+                {isCopied ? (
+                  <TiTickOutline className="text-green-400" />
+                ) : (
+                  <RxClipboardCopy />
+                )}
+              </button>
             </div>
           </div>
 
           {/* Stats */}
-          <div className="flex flex-wrap justify-between mt-10">
-            <div className="flex items-center gap-2 border border-gray-600 px-3 py-[0.35rem] rounded-lg">
-              <span className="text-sm font-bold text-gray-400 bg-gray-800 rounded-md px-3">
-                stars
-              </span>
-              <span className="text-sm font-bold text-white">2.4k</span>
-            </div>
-            <div className="flex items-center gap-2 border border-gray-600 px-3 py-[0.35rem] rounded-lg">
-              <span className="text-md font-bold text-gray-400 bg-gray-800 rounded-md px-3">
-                coverage
-              </span>
-              <span className="text-sm font-bold text-green-400">100%</span>
-            </div>
-            <div className="flex items-center gap-2 border border-gray-600 px-3 py-[0.35rem] rounded-lg">
-              <span className="text-md font-bold text-gray-400 bg-gray-800 rounded-md px-3">
-                license
-              </span>
-              <span className="text-sm font-bold text-white">MIT</span>
-            </div>
+          <div className="flex gap-4 mt-4 md:mt-10">
+            <Link
+              href="https://github.com/dewdrip/scaffold-eth-mobile/stargazers"
+              target="_blank"
+            >
+              <div className="flex items-center gap-2 bg-gray-100 border hover:bg-gray-200 px-3 py-[0.35rem] rounded-lg">
+                <span className="text-sm text-gray-600 font-bold bg-white rounded-md px-2">
+                  stars
+                </span>
+                <span className="text-sm font-bold">11</span>
+              </div>
+            </Link>
+
+            <Link
+              href="https://github.com/dewdrip/scaffold-eth-mobile/blob/main/LICENCE"
+              target="_blank"
+            >
+              <div className="flex items-center gap-2 bg-gray-100 border hover:bg-gray-200 px-3 py-[0.35rem] rounded-lg">
+                <span className="text-sm text-gray-600 font-bold bg-white rounded-md px-3">
+                  license
+                </span>
+                <span className="text-sm font-bold">MIT</span>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
 
       {/* Feature grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-16">
-        <div className="p-6 bg-gray-800/30 rounded-lg backdrop-blur-sm">
-          <h3 className="text-xl font-semibold text-white mb-2">Modular</h3>
+        <div className="p-6 bg-transparent border border-gray-200 rounded-lg backdrop-blur-sm">
+          <h3 className="text-lg font-bold mb-2">Modular</h3>
           <p className="text-gray-400">
             Composable modules to build applications & libraries with speed
           </p>
         </div>
-        <div className="p-6 bg-gray-800/30 rounded-lg backdrop-blur-sm">
-          <h3 className="text-xl font-semibold text-white mb-2">Lightweight</h3>
+        <div className="p-6 bg-transparent border border-gray-200 rounded-lg backdrop-blur-sm">
+          <h3 className="text-lg font-bold mb-2">Lightweight</h3>
           <p className="text-gray-400">
             Tiny bundle size optimized for tree-shaking
           </p>
         </div>
-        <div className="p-6 bg-gray-800/30 rounded-lg backdrop-blur-sm">
-          <h3 className="text-xl font-semibold text-white mb-2">Performant</h3>
+        <div className="p-6 bg-transparent border border-gray-200 rounded-lg backdrop-blur-sm">
+          <h3 className="text-lg font-bold mb-2">Performant</h3>
           <p className="text-gray-400">
-            Optimized architecture compared to alternative libraries
+            Optimized architecture for seamless experience across a wide range
+            of devices
           </p>
         </div>
-        <div className="p-6 bg-gray-800/30 rounded-lg backdrop-blur-sm">
-          <h3 className="text-xl font-semibold text-white mb-2">Typed APIs</h3>
+        <div className="p-6 bg-transparent border border-gray-200 rounded-lg backdrop-blur-sm">
+          <h3 className="text-lg font-bold mb-2">Typed Utils</h3>
           <p className="text-gray-400">
-            Flexible programmatic APIs with extensive TypeScript typing
+            Flexible programmatic Utils with extensive TypeScript typing
           </p>
         </div>
       </div>
