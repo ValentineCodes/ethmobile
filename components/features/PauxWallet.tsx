562fcdf935790f523ff25921ee724789e1408936
Valentine Orga <valentineorga@gmail.com>
2025-02-27 12:34:17 +0100

    feat: add paux wallet image

--- components/features/PauxWallet.tsx
+++ components/features/PauxWallet.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import { motion } from "framer-motion";
+import { IoWalletOutline } from "react-icons/io5";
+import Image from "next/image";
 
 type Props = {};
 
@@ -22,7 +24,7 @@ export default function PauxWallet({}: Props) {
           transition={{ duration: 0.7, ease: "easeInOut", delay: 0.2 }}
           className="flex flex-col items-center md:items-start gap-2"
         >
-          <h1 className="text-5xl font-extrabold">P</h1>
+          <IoWalletOutline className="text-5xl font-extrabold" />
           <h1 className="text-2xl font-extralight">PAUX WALLET</h1>
         </motion.div>
 
@@ -40,8 +42,8 @@ export default function PauxWallet({}: Props) {
           transition={{ duration: 0.7, ease: "easeInOut", delay: 0.35 }}
         >
           <p className="text-4xl text-gray-400 max-w-sm text-center md:text-left">
-            Customize your OWN <span className="whitespace-nowrap">in-app</span>{" "}
-            wallet
+            Customizable, and Lightweight{" "}
+            <span className="whitespace-nowrap">in-app</span> wallet
           </p>
         </motion.div>
 
@@ -59,9 +61,8 @@ export default function PauxWallet({}: Props) {
           transition={{ duration: 0.7, ease: "easeInOut", delay: 0.35 }}
         >
           <p className="text-lg text-gray-400 max-w-xs text-center md:text-left">
-            Consequat aute deserunt culpa officia esse aliquip tempor esse
-            nostrud officia. Anim eu elit eiusmod consequat laboris veniam
-            dolore.
+            Manage funds, sign transactions, and interact with EVM-compatible
+            chains
           </p>
         </motion.div>
       </div>
@@ -79,8 +80,26 @@ export default function PauxWallet({}: Props) {
             scale: 1,
           }}
           transition={{ duration: 0.7, ease: "easeInOut" }}
-          className="border border-white rounded-md shadow-[-3px_-3px_10px_2px_#15f7d6] w-full max-w-[500px] mt-12 md:mt-0"
-        ></motion.div>
+          className="w-full max-w-[500px] mt-12 md:mt-0 flex justify-center ml-10 md:ml-0"
+        >
+          <div className="relative w-[200px] md:w-[300px] aspect-square rounded-xl shadow-[0px_0px_4px_0px_#15f7d6]">
+            <Image
+              alt="Wallet Metadata"
+              src="/images/wallet_metadata.png"
+              fill
+              className="rounded-xl"
+            />
+
+            <div className="absolute top-[100px] right-[160px] md:top-[150px] md:right-[250px] w-[150px] md:w-[200px] aspect-square rounded-xl shadow-[0px_0px_4px_0px_#15f7d6]">
+              <Image
+                alt="Wallet Metadata"
+                src="/images/wallet_features.png"
+                fill
+                className="rounded-xl"
+              />
+            </div>
+          </div>
+        </motion.div>
       </div>
     </section>
   );
